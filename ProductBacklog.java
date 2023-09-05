a696b823d23fb075749188781a3eb1afebe809ac
jetxshen <127771410+jetxshen@users.noreply.github.com>
2023-09-05 17:40:54 +0800

    task information done

--- ProductBacklog.java
+++ ProductBacklog.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ProductBacklog {
+    Scanner scanner = new Scanner();
+    private ArrayList<Task> tasks;
+
+    public ProductBacklog() {
+        this.tasks = new ArrayList<>();
+    }
+    public ArrayList<Task> addTask(){
+
+    }
 }
--- Task.java
+++ Task.java
@@ -1,2 +1,63 @@
+
 public class Task {
+    private String name;
+    private Category category;
+    public enum Category{
+        Story,
+        Bug
+    }
+    private String assignee;
+    private Priority priority;
+    public enum Priority{
+        Low,
+        Medium,
+        Important,
+        Urgent
+    }
+    private Tag tag;
+    public enum Tag{
+        FrontEnd,
+        BackEnd,
+        API,
+        Database,
+        Framework,
+        Testing,
+        UI,
+        UX
+    }
+    private String description;
+    private Status status;
+    public enum Status{
+        Not_Started,
+        In_Progress,
+        Completed
+    }
+    private int numericalStoryPoint;
+    private CurrentStage currentStage;
+    public enum CurrentStage{
+        Planning,
+        Development,
+        Testing,
+        Integration
+    }
+
+    public Task(String name,
+                Category category,
+                String assignee,
+                Priority priority,
+                Tag tag,
+                String description,
+                Status status,
+                int numericalStoryPoint,
+                CurrentStage currentStage) {
+        this.name = name;
+        this.category = category;
+        this.assignee = assignee;
+        this.priority = priority;
+        this.tag = tag;
+        this.description = description;
+        this.status = status;
+        this.numericalStoryPoint = numericalStoryPoint;
+        this.currentStage = currentStage;
+    }
 }
