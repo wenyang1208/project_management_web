93f1677348abd0369d12884d6d215e3e6cd8cba1
jetxshen <127771410+jetxshen@users.noreply.github.com>
2023-09-12 19:24:10 +0800

    changed checkbox to radio for status

--- Product_Backlog/addTask.js
+++ Product_Backlog/addTask.js
@@ -10,22 +10,40 @@ document.addEventListener("DOMContentLoaded", function () {
         const taskStageSelect = document.getElementById("stages");
         const taskAssigneeChosen = document.getElementById("asignees");
 
-        // Get task name and description
+        const radioNotStarted = document.getElementById("ns");
+        const radioInProgress = document.getElementById("ip");
+        const radioCompleted = document.getElementById("c");
+
+        // Get task inputs
         const taskName = taskNameInput.value;
         const taskDescription = taskDescriptionInput.value;
         const taskPriority = taskPrioritySelect.value;
         const taskStage = taskStageSelect.value;
         const taskAssignee = taskAssigneeChosen.value;
+        let taskStatus = "";
+
+        // Determine and select Status
+        if (radioNotStarted.checked){
+            taskStatus = radioNotStarted.value
+        } else if (radioInProgress.checked){
+            taskStatus = radioInProgress.value
+        } else if (radioCompleted.checked){
+            taskStatus = radioCompleted.value
+        }
 
         // Add the new task to the savedTasks array
-        if (taskName.trim() !== "") {
+        if (taskName.trim() !== "" && taskStatus!== "") {
             // Load existing tasks from local storage
             const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
             
             // Add the new task
             savedTasks.push({ name: taskName, 
                 description: taskDescription, 
-                priority: taskPriority, stage: taskStage});
+                priority: taskPriority, 
+                stage: taskStage, 
+                assignee: taskAssignee, 
+                status: taskStatus,
+            });
 
             // Save the updated tasks array to local storage
             localStorage.setItem("tasks", JSON.stringify(savedTasks));
