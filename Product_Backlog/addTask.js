fb2886b401fcdc9d60e1a80625a21536831254ea
jetxshen <127771410+jetxshen@users.noreply.github.com>
2023-09-12 18:52:56 +0800

    added for assignee

--- Product_Backlog/addTask.js
+++ Product_Backlog/addTask.js
@@ -1,29 +1,6 @@
 // To receive the input and display it
 document.addEventListener("DOMContentLoaded", function () {
-    const addTaskButton = document.getElementById("add-button");
-    const taskInputPage = document.getElementById("task-input-page");
     const saveTaskButton = document.getElementById("saveTaskButton");
-    const taskList = document.getElementById("taskList");
-
-    // Load tasks from local storage on page load
-    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-    // Function to display tasks
-    function displayTasks() {
-        taskList.innerHTML = "";
-        savedTasks.forEach(function (task) {
-            const taskItem = document.createElement("div");
-            taskItem.innerHTML = `<strong>${task.name}</strong>
-            <br>${task.description}
-            <br><em>Priority: ${task.priority}</em>`;
-            taskList.appendChild(taskItem);
-        });
-    }
-
-    // Click event for "Add Task" button
-    addTaskButton.addEventListener("click", function () {
-        taskInputPage.style.display = "block"; // Show the task input page
-    });
 
     // Click event for "Save" button
     saveTaskButton.addEventListener("click", function () {
@@ -31,15 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
         const taskDescriptionInput = document.getElementById("taskDescription");
         const taskPrioritySelect = document.getElementById("taskPriority");
         const taskStageSelect = document.getElementById("stages");
+        const taskAssigneeChosen = document.getElementById("asignees");
 
         // Get task name and description
         const taskName = taskNameInput.value;
         const taskDescription = taskDescriptionInput.value;
         const taskPriority = taskPrioritySelect.value;
         const taskStage = taskStageSelect.value;
+        const taskAssignee = taskAssigneeChosen.value;
 
         // Add the new task to the savedTasks array
         if (taskName.trim() !== "") {
+            // Load existing tasks from local storage
+            const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+            
+            // Add the new task
             savedTasks.push({ name: taskName, 
                 description: taskDescription, 
                 priority: taskPriority, stage: taskStage});
@@ -47,18 +30,8 @@ document.addEventListener("DOMContentLoaded", function () {
             // Save the updated tasks array to local storage
             localStorage.setItem("tasks", JSON.stringify(savedTasks));
 
-            // Display tasks
-            displayTasks();
-
-            // Clear input fields
-            taskNameInput.value = "";
-            taskDescriptionInput.value = "";
-
-            // Hide the task input page
-            taskInputPage.style.display = "none";
+            // Navigate back to the index.html page
+            window.location.href = "productBacklog.html";
         }
     });
-
-    // Display tasks on page load
-    displayTasks();
 });
