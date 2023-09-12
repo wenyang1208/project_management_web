51a8f07da3fea2190171d6f2d2348ea1a8d970fd
jetxshen <127771410+jetxshen@users.noreply.github.com>
2023-09-12 18:25:47 +0800

    linked taskManager.js to productBacklog.html

--- Product_Backlog/taskManager.js
+++ Product_Backlog/taskManager.js
@@ -2,4 +2,27 @@
 document.addEventListener("DOMContentLoaded", function(){
     const addTaskButton = document.getElementById("add-button");
     const taskList = document.getElementById("taskList");
+
+    // Load tasks from local storage on page load
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    // Function to display tasks
+    function displayTasks() {
+        taskList.innerHTML = "";
+        savedTasks.forEach(function (task) {
+            const taskItem = document.createElement("div");
+            taskItem.innerHTML = `<strong>${task.name}</strong>
+                                    <br>${task.description}
+                                    <br><em>Priority: ${task.priority}</em>`;
+            taskList.appendChild(taskItem);
+        });
+    }
+
+    // Click event for "Add Task" button
+    addTaskButton.addEventListener("click", function () {
+        window.location.href = "addtask.html"; // Navigate to the addtask.html page
+    });
+
+    // Display tasks on page load
+    displayTasks();
 })
